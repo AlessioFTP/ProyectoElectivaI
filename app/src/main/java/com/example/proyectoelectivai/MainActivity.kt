c23c2d0f1263aa5f7dbaea934093c8dde936cf36
AlessioFTP <jrojas78@uan.edu.co>
2025-09-21 18:28:07 -0500

    Fondo estatico de la app

--- app/src/main/java/com/example/proyectoelectivai/MainActivity.kt
+++ app/src/main/java/com/example/proyectoelectivai/MainActivity.kt
@@ -4,6 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import com.example.proyectoelectivai.componentes.BarraNavegacion
 import com.example.proyectoelectivai.ui.theme.ProyectoElectivaITheme
 
@@ -12,8 +18,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ProyectoElectivaITheme {
-                BarraNavegacion()
+            Box(modifier = Modifier.fillMaxSize()) {
+
+                Image(
+                    painter = painterResource(id = R.drawable.fondoapp),
+                    contentDescription = null,
+                    modifier = Modifier.matchParentSize(),
+                    contentScale = ContentScale.Crop
+                )
+
+                ProyectoElectivaITheme {
+                    BarraNavegacion()
+                }
             }
         }
     }
