065a2ccae30197c5d3e1c4bc6eddedbc84319d63
AlessioFTP <jrojas78@uan.edu.co>
2025-09-23 16:38:05 -0500

    Vista pantalla mis guias terminada

--- app/src/main/java/com/example/proyectoelectivai/pantallas/PantallaMisGuias.kt
+++ app/src/main/java/com/example/proyectoelectivai/pantallas/PantallaMisGuias.kt
@@ -2,24 +2,65 @@ package com.example.proyectoelectivai.pantallas
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.proyectoelectivai.componentes.TarjetaMiGuia
 
 
 @Composable
 fun PantallaMisGuias(navController: NavController, modifier: Modifier = Modifier) {
     Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 30.dp)
+            .verticalScroll(rememberScrollState())
     ) {
-        Text(text = "Mis Guias")
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            TarjetaMiGuia(titulo = "Guia de balatro", juego = "balatro")
+            TarjetaMiGuia(titulo = "Guia para principiantes 7 days", juego = "7 days to die")
+        }
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            TarjetaMiGuia(titulo = "Como subir de rango en futchampions", juego = "fc26")
+            TarjetaMiGuia(titulo = "Guia para speedrun", juego = "minecraft")
+        }
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            TarjetaMiGuia(titulo = "Como farmear la petrolera facil", juego = "rust")
+            TarjetaMiGuia(titulo = "Guia de construcción avanzada", juego = "7 days to die")
+        }
 
     }
+
 }
 
 
